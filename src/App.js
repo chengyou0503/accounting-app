d8a51e584a3e3dc42aa4db460937238c300af6ea
chengyou0503 <99228903+chengyou0503@users.noreply.github.com>
2025-11-08 01:32:03 +0800

    更新：更新 GAS API URL

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const { Header, Content } = Layout;
 
 // ==================================================================
 // 你的 Google Apps Script API 網址！
-const API_URL = "https://script.google.com/macros/s/AKfycby9VivHMPOacjuCY5ECK0Xcu7A0xuNPyHyoKEaxEZxdofURszkFSnNo1y4RrtkZmFMy/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwGocp0OfMBIxFVd-fJu5pTUvF0r6RqqGVDcDG9KbwUEwjNbo0bPWQVfKOEzgtpp9dv/exec";
 // ==================================================================
 
 const cuteTheme = {
