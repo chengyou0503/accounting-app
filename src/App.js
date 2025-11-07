de68c73d40ac492ff6c140c790a63d39ab79fb5b
chengyou0503 <99228903+chengyou0503@users.noreply.github.com>
2025-11-07 18:45:09 +0800

    更新 API 網址並啟用測試模式

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const { Header, Content } = Layout;
 
 // ==================================================================
 // 你的 Google Apps Script API 網址！
-const API_URL = "https://script.google.com/macros/s/AKfycbwQ3nE2WRTLYpPdthi83ashP-c2DvGP03OOvTwu7pNjX7jX1P1raBZuZ1phmcI70TGJ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxUee3DrWobvPClM5JLXo8ZhXDGGXGYOO-tYDiDmsJLkIgIVgf8FHgFKse_ggTom4Ha/exec";
 // ==================================================================
 
 const cuteTheme = {
